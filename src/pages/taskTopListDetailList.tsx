6818599a3b5ef85c32030827df633e5b90a42f18
chenyun <yun160308@gmail.com>
2021-07-09 22:27:38 +0800

    增加对榜单爬虫的支持

--- src/pages/taskTopListDetailList.tsx
+++ src/pages/taskTopListDetailList.tsx
@@ -1,36 +1,51 @@
 import React, { Component } from 'react';
-import { Button, Descriptions, Input, List, message } from 'antd';
+import { Button, Descriptions, Input, List, message, Select } from 'antd';
 import axios from 'axios';
 import { Link } from 'umi';
+
+const { Option } = Select;
 
 class TaskTopListDetailList extends Component {
 
   state = {
     topTaskId: '',
     topListDetailInfoList: [],
+    topListUpdateTime: '',
+    topListDateList: ['全部'],
   };
 
   componentWillMount() {
     const { location } = this.props;
     this.state.topTaskId = location.query.topTaskId;
+    this.getTopListDateList();
+  }
+
+  getTopListDateList() {
+    const { topListDateList } = this.state;
+    axios.get('/admin/api/getTopListAllDate').then((httpResponse) => {
+
+      topListDateList.push(httpResponse.data.data);
+      this.setState({ topListDateList: topListDateList });
+
+    }).catch((error) => {
+      message.info(error.string);
+    });
   }
 
   render() {
-    const { topListDetailInfoList, topTaskId } = this.state;
+    const { topListDetailInfoList, topTaskId, topListDateList } = this.state;
 
     const onChange = (e) => {
       this.setState({ topTaskId: e.target.value });
     };
 
     const onClick = () => {
-      const { topTaskId } = this.state;
+      const { topTaskId, topListUpdateTime } = this.state;
 
-      if (topTaskId === '') {
-        return;
-      }
       axios.get('/admin/api/getTaskTopListDetailList', {
         params: {
           topTaskId: topTaskId,
+          topListUpdateTime: topListUpdateTime,
         },
       }).then((httpResponse) => {
         if (!httpResponse.data.data) {
@@ -38,14 +53,27 @@ class TaskTopListDetailList extends Component {
         } else {
           this.setState({ topListDetailInfoList: httpResponse.data.data });
         }
-
       }).catch((error) => {
         message.info(error.string);
       });
     };
+
+    const handleChange = (value) => {
+      this.state.topListUpdateTime = value;
+    };
+
     return <div>
 
       <Input prefix={<span>TopTaskId：</span>} defaultValue={topTaskId} placeholder={'TopTaskId'} onChange={onChange} />
+      <div style={{ marginTop: 20 }}>
+        <span>更新时间: </span>
+        <Select defaultValue={topListDateList[0] ? topListDateList[0] : ''} style={{ width: 300 }}
+                onChange={handleChange}>
+          {topListDateList.map(item => {
+            return <Option value={item}>{item}</Option>;
+          })}
+        </Select>
+      </div>
       <Button style={{ marginTop: 20 }} type={'primary'} onClick={onClick}>搜索</Button>
       <List
         style={{ marginTop: 20 }}
