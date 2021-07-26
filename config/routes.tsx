43a57a71613f001cd86c4b807031069c6af9b9a0
chenyun <yun160308@gmail.com>
2021-07-26 20:50:30 +0800

    bug fix

--- config/routes.tsx
+++ config/routes.tsx
@@ -7,31 +7,37 @@ export default [
         path: '/',
         name: '爬虫数据总揽',
         component: '@/pages/index',
+        showInMenu: true,
       },
       {
         path: '/taskList',
         name: '爬虫任务信息',
         component: '@/pages/taskList',
+        showInMenu: true,
       },
       {
         path: '/taskScoreList',
         name: '爬虫曲谱信息',
         component: '@/pages/taskScoreList',
+        showInMenu: true,
       },
       {
         path: '/taskTopListDetailList',
         name: '爬虫榜单信息',
         component: '@/pages/taskTopListDetailList',
+        showInMenu: true,
       },
       {
         path: '/scoreSearch',
         name: '曲谱搜索',
         component: '@/pages/scoreSearch',
+        showInMenu: true,
       },
       {
         path: '/scoreDetail',
         name: '曲谱详情',
         component: '@/pages/scoreDetail',
+        showInMenu: false,
       },
     ],
 
--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import { Link } from 'umi';
 import ProLayout, { PageContainer } from '@ant-design/pro-layout';
-import {Card} from 'antd';
+import { Card } from 'antd';
 import styles from './index.less';
 
 class IndexLayout extends React.Component {
 
   render() {
     const { children, location, route } = this.props;
-
+    const newRoutes = route.routes.filter(item => item.showInMenu);
+    route.routes = newRoutes;
     return (
       <div className={styles.ProLayoutDev}>
         <ProLayout
-          title={"SOUPU-爬虫"}
+          title={'SOUPU-爬虫'}
           location={location}
           route={route}
           menuItemRender={(menuItemProps, defaultDom) => {
